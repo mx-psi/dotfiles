d4ec3266522e62378352cb764df70a5157e4e23d
Pablo <mx-psi@users.noreply.github.com>
2019-08-06 21:53:43 +0200

    Moves backups to FTP

--- nextcloud-backup.py
+++ nextcloud-backup.py
@@ -4,35 +4,43 @@ import yaml
 import requests
 import datetime
 import os.path
+import tempfile
+import ftplib
 
-
-def get_credentials(filepath):
+def load_yaml(filepath):
     """Gets the credentials from secret YAML file."""
     with open(filepath, 'r') as f:
         return yaml.load(f.read())
 
 
 def backup_name(calendar, parent):
-    return parent + "/" + calendar + "-" + datetime.date.today().isoformat(
+    return  parent + "/Backups/" + calendar + "-" + datetime.date.today().isoformat(
     ) + ".ics"
 
+def upload_to_ftp(ftp, filename, contents):
+  with tempfile.TemporaryFile() as temp:
+    temp.write(bytes(contents, encoding="utf-8"))
+    temp.seek(0)
+    ftp.storlines("STOR {}".format(filename), temp)
 
-def backup_calendars(data, dst):
+
+def backup_calendars(data, ftp_data):
     """Backup the listed calendars."""
     url = "https://{host}/remote.php/dav/calendars/{username}/{calendar}?export"
-    for calendar in data["calendars"]:
+    with ftplib.FTP(ftp_data["url"]) as ftp:
+      ftp.login()
+      for calendar in data["calendars"]:
         response = requests.get(url.format(host=data["host"],
                                            username=data["username"],
                                            calendar=calendar),
                                 auth=(data["username"], data["password"]))
 
         if response.status_code != 200:
-            continue  # Failed backup
-
-        with open(backup_name(calendar, dst), 'w+') as f:
-            f.write(response.text)
+          continue  # Failed backup
+        upload_to_ftp(ftp, backup_name(calendar, ftp_data["folder"]), response.text)
 
 
 if __name__ == "__main__":
-    credentials = get_credentials(os.path.expanduser("~/dotfiles/secrets/nextcloud-secrets.yaml"))
-    backup_calendars(credentials, dst=os.path.expanduser("~/Backups"))
+    credentials = load_yaml(os.path.expanduser("~/dotfiles/secrets/nextcloud-secrets.yaml"))
+    ftp_data = load_yaml(os.path.expanduser("~/dotfiles/secrets/ftp-secrets.yaml"))
+    backup_calendars(credentials, ftp_data)

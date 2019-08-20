3f7f24662dff58cd5ef0878e697d554b8f7bc673
Pablo <mx-psi@users.noreply.github.com>
2019-08-20 13:11:32 +0200

    Fixes bug when there are no files to modify

--- hooks/yapf-hook.py
+++ hooks/yapf-hook.py
@@ -17,12 +17,13 @@ def get_modified_files():
 if __name__ == "__main__":
     files = [path for path in get_modified_files() if path.endswith(".py")]
 
-    # Get the diff
-    diff = subprocess.run(["yapf", "-d", "-p", "--style", "google"] + files,
-                          stdout=subprocess.PIPE)
+    if files:
 
-    # patch differences if there are any
-    if diff.stdout:
-        subprocess.run(["patch", "-p0"], input=diff.stdout)
-        print("Files were changed, aborting commit.")
-        exit(-1)
+        # Get the diff
+        diff = subprocess.run(["yapf", "-d", "-p", "--style", "google"] + files,
+                              stdout=subprocess.PIPE)
+
+        if diff.stdout:
+            subprocess.run(["patch", "-p0"], input=diff.stdout)
+            print("Files were changed, aborting commit.")
+            exit(-1)

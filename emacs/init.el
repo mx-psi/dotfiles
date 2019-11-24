23c6de04d2391a3acf2bfdcc17ac35243148f69f
Pablo Baeyens <pbaeyens31+github@gmail.com>
2019-11-24 13:37:36 +0100

    Organizes init.el

--- emacs/init.el
+++ emacs/init.el
@@ -20,31 +20,43 @@
 
 ;; EMACS CONFIG ;;
 
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message nil)
-(setq initial-major-mode 'markdown-mode)
+;; Aesthetics
+(setq
+  inhibit-startup-screen t
+  initial-scratch-message nil
+  initial-major-mode 'markdown-mode
+  resize-mini-windows nil)
+
+(setq-default
+  indicate-empty-lines t
+  show-trailing-whitespace t
+  word-wrap 1
+  fill-column 80)
+
+
 (pending-delete-mode 1)             ;; Delete selection when typing
 (global-auto-revert-mode 1)         ;; Auto refresh
-(setq resize-mini-windows nil)
 (global-visual-line-mode t)
-(setq-default indicate-empty-lines t
-  show-trailing-whitespace t)
+
+;; Backups
+(setq
+  backup-directory-alist '(("." . "~/.emacs.d/backups"))
+  delete-old-versions -1
+  version-control t
+  vc-make-backup-files t
+  auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+(defalias 'yes-or-no-p #'y-or-n-p) ;; honestly who says "yes" nowadays?
 
 
 ;; Run only when the frame has been created (for emacsclient)
 (defun set-frame (_)
   (setq frame-title-format "%f")
-  (set-frame-font "Iosevka 12" nil t)
-  ;; Minimalistic setup (No startup, scratch-message, toolbar,menu,scroll bar or tooltip)
+  (set-frame-font "Fira Code 12" nil t)
   (tool-bar-mode -1)
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (tooltip-mode -1)
-  ;; Sensible defaults
-  (setq make-backup-files nil)        ;; No backups
-  (setq-default word-wrap 1)          ;; Wrap words
-  (setq-default fill-column 80)
-  (defalias 'yes-or-no-p #'y-or-n-p) ;; honestly who says "yes" nowadays?
 )
 
 (add-to-list 'after-make-frame-functions #'set-frame)
@@ -60,7 +72,39 @@
 
 (setq exec-path (append exec-path '("~/.cabal/bin")))
 
+;; Git
+
+(setq vc-follow-symlinks t)
+
 ;; PACKAGES ;;
+
+(use-package diminish :ensure t)
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind ("M-x" . smex)
+  )
+
+(use-package ido
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (ido-mode 1)
+    (ido-everywhere 1)
+    )
+  )
+
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :init
+  (progn
+    (use-package smartparens-config)
+    (smartparens-global-mode 1)
+    (show-smartparens-global-mode 1))
+  :config (setq smartparens-strict-mode t)
+  )
 
 ;; Theme
 
@@ -78,7 +122,7 @@
     '(:logic :nil))
   (pretty-activate-groups
     '(:greek :arithmetic-nary :punctuation))
-  (pretty-deactivate-patterns '(:circ :++ :sum :product)))
+  (pretty-deactivate-patterns '(:circ :++ :sum :product :equality :==)))
   )
 
 
@@ -94,14 +138,16 @@
    ("C-c a" . org-agenda)
    ("C-c c" . org-capture))
   :config
-  (progn (setq org-catch-invisible-edits 'error) ;; No editing of folded regions
-    (setq org-startup-indented t) ;; Idented at startup
-    (setq org-log-done 'time) ;; Log completion time
-    (setq org-hierarchical-todo-statistics nil) ;; Stats are recursive
-    (setq org-support-shift-select t)
-    (setq org-directory "~/org")
-    (setq org-enforce-todo-dependencies t) ;; TODO dependencies are enforced
-    (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.6))
+  (progn
+    (setq
+      org-catch-invisible-edits 'error ;; No editing of folded regions
+      org-startup-indented t ;; Idented at startup
+      org-log-done 'time ;; Log completion time
+      org-hierarchical-todo-statistics nil ;; Stats are recursive
+      org-support-shift-select t
+      org-directory "~/org"
+      org-enforce-todo-dependencies t ;; TODO dependencies are enforced
+      org-format-latex-options (plist-put org-format-latex-options :scale 1.6))
    )
   )
 
@@ -145,21 +191,15 @@
   :ensure t
   :bind ("C-c f" . clang-format-region))
 
-;; Idris
+;; Misc languages
 
-(use-package idris-mode
-  :ensure t)
-
-
-;; Go
-(use-package go-mode
-  :ensure t
-  :mode "\\.go\\'"
-  )
+(use-package idris-mode :ensure t)
+(use-package go-mode :ensure t)
 
 
 ;; Editorconfig
 (use-package editorconfig
+  :diminish editorconfig-mode
   :ensure t
   :config
   (editorconfig-mode 1))
@@ -170,11 +210,6 @@
   :bind ("C-c r" . vr/replace)
   )
 
-;;(use-package yasnippet
-;;  :ensure t
-;;  :defer t
-;;  :init (yas-global-mode t)
-;;  )
 
 (use-package company
   :ensure t
@@ -207,42 +242,20 @@
   (add-to-list 'company-backends 'company-jedi))
 
 
-(use-package smex
-  :ensure t
-  :init (smex-initialize)
-  :bind ("M-x" . smex)
-  )
-
-(use-package ido
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (ido-mode 1)
-    (ido-everywhere 1)
-    )
-  )
-
-(use-package smartparens
-  :ensure t
-  :init
-  (progn
-    (use-package smartparens-config)
-    (smartparens-global-mode 1)
-    (show-smartparens-global-mode 1))
-  :config (setq smartparens-strict-mode t)
- )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (go-mode gnu-elpa-keyring-update yasnippet yaml-mode visual-regexp use-package spacemacs-theme smex smartparens smart-mode-line rust-mode pretty-mode powerline pandoc-mode org-bullets mustache-mode monokai-theme mediawiki markdown-mode magit lex jekyll-modes jedi ir-black-theme idris-mode hlint-refactor haskell-mode guess-language flycheck ess emojify editorconfig drag-stuff dracula-theme dockerfile-mode csv-mode csharp-mode company-jedi clips-mode clang-format cdlatex auctex))))
+  '(custom-safe-themes
+     (quote
+       ("a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" default)))
+  '(package-selected-packages
+     (quote
+       (nordless-theme go-mode gnu-elpa-keyring-update yasnippet yaml-mode visual-regexp use-package spacemacs-theme smex smartparens smart-mode-line rust-mode pretty-mode powerline pandoc-mode org-bullets mustache-mode monokai-theme mediawiki markdown-mode magit lex jekyll-modes jedi ir-black-theme idris-mode hlint-refactor haskell-mode guess-language flycheck ess emojify editorconfig drag-stuff dracula-theme dockerfile-mode csv-mode csharp-mode company-jedi clips-mode clang-format cdlatex auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((((class color) (min-colors 89)) (:foreground "#D8DEE9" :background nil)))))

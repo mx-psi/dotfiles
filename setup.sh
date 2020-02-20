97efcdb71ac9755d6c18f3bbeb8bdafb4255c1f6
Pablo Baeyens <pbaeyens31+github@gmail.com>
2020-02-20 21:14:34 +0100

    Lints using shellcheck

--- setup.sh
+++ setup.sh
@@ -9,22 +9,22 @@ declare -A LINK_NAMES=(
 ["backups/general-backup.py"]="/etc/cron.daily/backup"
 ["backups/nextcloud-backup.py"]="/etc/cron.monthly/nextcloud-backup"
 ["backups/pocket-backup.py"]="/etc/cron.daily/pocket-backup"
-["bashrc"]="/home/`whoami`/.bashrc"
-["emacs/editorconfig"]="/home/`whoami`/.editorconfig"
-["emacs/snippets"]="/home/`whoami`/.emacs.d/snippets"
-["emacs/init.el"]="/home/`whoami`/.emacs.d/init.el"
-["gitconfig"]="/home/`whoami`/.gitconfig"
-["hidden"]="/home/`whoami`/.hidden"
-["inputrc"]="/home/`whoami`/.inputrc"
+["bashrc"]="/home/$(whoami)/.bashrc"
+["emacs/editorconfig"]="/home/$(whoami)/.editorconfig"
+["emacs/snippets"]="/home/$(whoami)/.emacs.d/snippets"
+["emacs/init.el"]="/home/$(whoami)/.emacs.d/init.el"
+["gitconfig"]="/home/$(whoami)/.gitconfig"
+["hidden"]="/home/$(whoami)/.hidden"
+["inputrc"]="/home/$(whoami)/.inputrc"
 )
 
 for dotfile in "${SU_DOTFILES[@]}"
 do
-  sudo ln -sbv `pwd`/$dotfile ${LINK_NAMES[$dotfile]}
+  sudo ln -sbv "$(pwd)/$dotfile" "${LINK_NAMES[$dotfile]}"
 done
 
 
 for dotfile in "${DOTFILES[@]}"
 do
-  ln -sbv `pwd`/$dotfile ${LINK_NAMES[$dotfile]}
+  ln -sbv "$(pwd)/$dotfile" "${LINK_NAMES[$dotfile]}"
 done
